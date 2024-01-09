82bb6e742a2b762b6f8a71466deaf5228dc4c89e
taewoo <rlaxodn322@gmail.com>
2024-01-09 16:26:19 +0900

    Carousel margin 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,6 @@ import Head from 'next/head';
 import MainLayout from '../layouts';
 import { Page } from './style';
 
-import App from '../components/antd/card';
-import MapComponent from '../components/apis/kakao/map';
-import Main from '../components/Main/index';
 import Carousel from '../components/antd/carousel';
 const Home = () => {
   return (
@@ -13,9 +10,11 @@ const Home = () => {
         <title>TIGER HOUSE</title>
         <meta name="description" content="text." />
       </Head>
-      <Carousel />
+      <div style={{ marginTop: '30px' }}>
+        <Carousel />
+      </div>
+
       <Page>
-        {' '}
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <div style={{ border: '1px solid black', width: '685px', height: '50vh' }}></div>
           <div style={{ border: '1px solid black', width: '685px', height: '50vh' }}></div>
