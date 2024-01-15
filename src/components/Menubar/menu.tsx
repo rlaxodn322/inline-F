9931923884b328c6a089ed75c80ce16c2c8bd92d
taewoo <rlaxodn322@gmail.com>
2024-01-15 12:14:34 +0900

    메뉴 수정

--- src/components/Menubar/menu.tsx
+++ src/components/Menubar/menu.tsx
@@ -3,32 +3,26 @@ import Link from 'next/link';
 
 const menuItems = [
   { href: '/auth/page1', label: '인라인 스피드', subMenu: ['히스토리', '강사진', '커리큘럼'] },
-  { href: '/auth/page2', label: '스키', subMenu: ['서브메뉴4', '서브메뉴5', '서브메뉴6'] },
-  { href: '/auth/page3', label: '선수트레이닝& 부상 후 복귀 운동', subMenu: ['서브메뉴7', '서브메뉴8', '서브메뉴9'] },
-  { href: '/auth/page4', label: '자료실', subMenu: ['서브메뉴10', '서브메뉴11', '서브메뉴12'] },
-  { href: '/auth/page5', label: '1.', subMenu: ['서브메뉴13', '서브메뉴14', '서브메뉴15'] },
+  { href: '/auth/page2', label: '스키', subMenu: ['히스토리', '강사진', '커리큘럼'] },
+  { href: '/auth/page3', label: '선수트레이닝', subMenu: ['히스토리', '강사진', '커리큘럼'] },
+  { href: '/auth/page4', label: '부상 후 복귀 운동', subMenu: ['히스토리', '강사진', '커리큘럼'] },
+  { href: '/auth/page5', label: '매장', subMenu: ['인라인', '인라인용품'] },
   { href: '/auth/page6', label: '타이거하우스소개', subMenu: ['서브메뉴16', '서브메뉴17', '서브메뉴18'] },
-  { href: '/auth/page7', label: '오프라인매장', subMenu: ['인라인', '의류'] },
-  { href: '/auth/page8', label: '기타', subMenu: ['서브메뉴22', '서브메뉴23', '서브메뉴24'] },
 ];
 
 const MenuBar: React.FC = () => {
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
   const submenuRef = useRef<HTMLDivElement>(null);
+  const [submenuVisible, setSubmenuVisible] = useState(false);
 
   const handleMouseEnter = (index: number) => {
     setHoveredIndex(index);
+    setSubmenuVisible(true);
   };
 
-  const handleMouseLeave = (event: React.MouseEvent) => {
-    // Check if the mouse is still inside the submenu or the menu before closing
-    const relatedTarget = event.relatedTarget as Node;
-    if (
-      (submenuRef.current && relatedTarget && !submenuRef.current.contains(relatedTarget)) ||
-      (!submenuRef.current && !relatedTarget)
-    ) {
-      setHoveredIndex(null);
-    }
+  const handleMouseLeave = () => {
+    setHoveredIndex(null);
+    setSubmenuVisible(false);
   };
 
   return (
@@ -53,7 +47,6 @@ const MenuBar: React.FC = () => {
               </a>
             </Link>
 
-            {/* 서브메뉴를 동적으로 렌더링합니다. */}
             {index === hoveredIndex && item.subMenu && (
               <div
                 ref={submenuRef}
@@ -64,10 +57,13 @@ const MenuBar: React.FC = () => {
                   background: 'white',
                   boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
                   zIndex: 1,
-                  marginTop: '10px', // Adjust this value to create space between menu and submenu
-                  border: '1px solid #ccc', // Add a border
-                  borderRadius: '4px', // Add border radius for rounded corners
-                  minWidth: '150px', // Set a minimum width
+                  marginTop: '10px',
+                  border: '1px solid #ccc',
+                  borderRadius: '4px',
+                  minWidth: '180px',
+                  opacity: submenuVisible ? 1 : 0,
+                  transform: `translateY(${submenuVisible ? '0%' : '-20%'})`,
+                  transition: 'opacity 0.3s ease-in-out, transform 0.3s ease-in-out',
                 }}
               >
                 {item.subMenu.map((subItem, subIndex) => (
